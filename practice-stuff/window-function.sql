d3129af503a362589d9c0b6146da13b6f9a88633
Muhammad Bilal Shahid <muhammadbilalshahid@Muhammads-MacBook-Pro.local>
2026-07-30 12:58:45 +0500

    Window function basics

---
+++ practice-stuff/window-function.sql
@@ -0,0 +1,19 @@
+-- Window function is similar to do like ORDER BY
+-- Order by reduces the whole table or we can say it groups the rows for specific column mentioned.
+-- i.e; if there are 10 rows of specific column with same value it will become one row
+-- This is actually a mutation on original table
+-- But what If there is a requirement to do the opposite but get actual result ?
+-- In order to acheive that, window function comes to play
+
+
+-- Simplest way to acheive GROUP BY result
+SELECT e.student_id,
+    SUM(e.score) AS total_score
+FROM exam_scores AS e
+GROUP BY e.student_id;
+
+
+-- Now let's do syntax for window function
+SELECT *,
+    SUM(e.score) OVER(PARTITION BY student_id) AS total_sum
+FROM exam_scores AS e;
